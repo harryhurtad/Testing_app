3b7037e38f2d4cf3c6aadafb046bf9d81286a22e
harryhurtad <harryhurtad@gmail.com>
2015-01-15 21:23:16 -0500

    prueba 01 git

---
+++ src/testing/TestingMain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testing;
+
+/**
+ *
+ * @author JACKSERVER
+ */
+public class TestingMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    class A{
+    
+    }
+    class B extends A{
+    }
+}
